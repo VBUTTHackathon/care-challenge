5e191d061f99a09547195e69b22164f1e80cc45b
Wassim Ben Ayed <wbenayed@vistaprint.com>
2016-11-15 15:22:02 +0100

    update get users query  to retrieve only un-chosen and not the current user

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,11 +5,14 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+
 module.exports = {
   users: function (req, res){
-    return User.find()
+    return User.find({state:"none"})
           .then(function(users){
-            res.json({data:users});
+            res.json({data:users.filter(function(user){
+              return user.id != req.user.id;
+            })});
           });
   }
 };
