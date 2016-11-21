a1aab93c47d253e80caea2c5d91a3fa3aee05f86
phoewass <phoewass@users.noreply.github.com>
2016-11-21 23:56:49 +0100

    pass parner object to Homepage view

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -10,7 +10,12 @@ module.exports = {
     if(req.user.isAdmin){
       return res.redirect('/admin');
     }
-    return res.view('homepage',{state:req.user.state});
+    var state = req.user.state;
+    if(state !== "none"){
+      var partner = req.user.getPartner();
+      return res.view('homepage',{state:state,partner:partner});
+    }
+    return res.view('homepage',{state:state});
   },
   livecheck: function(req, res) {
     return res.send(200);
