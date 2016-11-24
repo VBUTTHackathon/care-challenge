d7ea8cb8173a8d653c4b8aa3c84f4b546961f68f
phoewass <phoewass@users.noreply.github.com>
2016-11-24 15:54:30 +0100

    script for adding users

---
+++ add-users.rb
@@ -0,0 +1,24 @@
+require "json"
+require 'csv'
+require 'net/http'
+
+
+  def post_http_request(url,content)
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host,uri.port)
+    request = Net::HTTP::Post.new(uri.path,{"Accept"=> "application/json"})
+    request.content_type='application/json'
+    request.body=content
+
+    http.request(request) do |resp|
+      #raise "Unable to access" unless resp.is_a?(Net::HTTPSuccess)
+      return resp.body
+    end
+  end
+
+agents = CSV.read('agents.csv',{ :col_sep => ';' })
+agents.each do |agent|
+  content = {:employeeId =>agent[0], :fullName =>agent[1], :team =>agent[2], :channel =>agent[3], :category => agent[4]}
+  post_http_request('http://localhost:1337/user/',content.to_json)
+  sleep(0.1)
+end
